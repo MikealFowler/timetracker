846825ca6f1f5755e0718d0a89ea1113b741e318
MikealFowler <mikeyfowler36@gmail.com>
2024-11-08 11:44:32 -0400

    can request now

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,35 @@
-var moment = require('moment-timezone');
-moment.tz.guess();
-
-var edmonton = moment.tz("America/Edmonton");
-var toronto = moment.tz("America/Toronto");
-var halifax = moment.tz("America/Halifax");
-var fortNelson = moment.tz('America/Fort_Nelson');
-var winnipeg = moment.tz('America/Winnipeg');
+const axios = require('axios');
+let destination = 'http://localhost:4000/' 
 
 
-//Print off to my console so I know each time zone is working
-console.log("Halifax: " + halifax.format());
-console.log("Toronto: " + toronto.format());
-console.log("Winnipeg: " + winnipeg.format());
-console.log("Edmonton: " + edmonton.format());
-console.log("Fort Nelson: " + fortNelson.format());
+// Send a POST request
+function submitUser(){
+    let newUser = { "name": document.getElementById("userName").value, "password": document.getElementById("newPassword").value };
+    axios.post(destination, newUser)
+        .then(response => {
+            console.log('User created:', response.data);
+        })
+        .catch(error => {
+            console.error('Error creating user:', error);
+        });
 
-//Starting time
-function updateTime() {
-    let newTimeElement = document.getElementById('timezoneSelect').value;
-    let currentTimeElement = document.getElementById('currentTimezone');
-    if (newTimeElement === "Halifax") {
-        let halifax = moment.tz("America/Halifax");
-        currentTimeElement.textContent = "Halifax " + halifax.format("HH:mm:ss");
-    } else if (newTimeElement === "Toronto") {
-        let toronto = moment.tz("America/Toronto");
-        currentTimeElement.textContent = "Toronto " + toronto.format("HH:mm:ss");
-    } else if (newTimeElement === "Winnipeg") {
-        let winnipeg = moment.tz('America/Winnipeg');
-        currentTimeElement.textContent = "Winnipeg " + winnipeg.format("HH:mm:ss");
-    } else if (newTimeElement === "Edmonton") {
-        let edmonton = moment.tz("America/Edmonton");
-        currentTimeElement.textContent = "Edmonton " + edmonton.format("HH:mm:ss");
-    } else if (newTimeElement === "Fort Nelson") {
-        let fortNelson = moment.tz('America/Fort_Nelson');
-        currentTimeElement.textContent = "Fort Nelson " + fortNelson.format("HH:mm:ss");
+};
+
+//send a get request for all users
+async function getUsers() {
+    try{
+        const response = await axios.get(destination);
+        console.log('users fetched:', response.data);
+        document.getElementById("allUsers").innerText = response.data
+    } catch (error) {
+        console.error('error fetching users', error.response ? error.response.data : error.message);
     }
+    
 }
 
+// Attach  to the global window object
 
-//run update every second
-setInterval(updateTime, 1000);
-
-
-// Attach changeTime to the global window object
-window.changeTime = changeTime;
+window.submitUser = submitUser;
+window.getUsers = getUsers;
 
 
---
+++ src/js/time.js
@@ -0,0 +1,43 @@
+var moment = require('moment-timezone');
+moment.tz.guess();
+
+var edmonton = moment.tz("America/Edmonton");
+var toronto = moment.tz("America/Toronto");
+var halifax = moment.tz("America/Halifax");
+var fortNelson = moment.tz('America/Fort_Nelson');
+var winnipeg = moment.tz('America/Winnipeg');
+
+
+//Print off to my console so I know each time zone is working
+console.log("Halifax: " + halifax.format());
+console.log("Toronto: " + toronto.format());
+console.log("Winnipeg: " + winnipeg.format());
+console.log("Edmonton: " + edmonton.format());
+console.log("Fort Nelson: " + fortNelson.format());
+
+//Starting time
+function updateTime() {
+    let newTimeElement = document.getElementById('timezoneSelect').value;
+    let currentTimeElement = document.getElementById('currentTimezone');
+    if (newTimeElement === "Halifax") {
+        let halifax = moment.tz("America/Halifax");
+        currentTimeElement.textContent = "Halifax " + halifax.format("HH:mm:ss");
+    } else if (newTimeElement === "Toronto") {
+        let toronto = moment.tz("America/Toronto");
+        currentTimeElement.textContent = "Toronto " + toronto.format("HH:mm:ss");
+    } else if (newTimeElement === "Winnipeg") {
+        let winnipeg = moment.tz('America/Winnipeg');
+        currentTimeElement.textContent = "Winnipeg " + winnipeg.format("HH:mm:ss");
+    } else if (newTimeElement === "Edmonton") {
+        let edmonton = moment.tz("America/Edmonton");
+        currentTimeElement.textContent = "Edmonton " + edmonton.format("HH:mm:ss");
+    } else if (newTimeElement === "Fort Nelson") {
+        let fortNelson = moment.tz('America/Fort_Nelson');
+        currentTimeElement.textContent = "Fort Nelson " + fortNelson.format("HH:mm:ss");
+    }
+}
+
+//run update every second
+setInterval(updateTime, 1000);
+
+window.changeTime = changeTime;
